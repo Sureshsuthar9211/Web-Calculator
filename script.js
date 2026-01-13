69593eb8da3dad05613923e8cdfe9e68acfa94d6
sureshsuthar9211 <sureshsuthar1137@gmail.com>
2026-01-13 23:16:21 +0530

    complete css

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ let input = document.getElementById('input-box')
 let buttons = document.querySelectorAll('button')
 
 let string = ""
-let array = ""
+let array = Array.from(buttons)
+
+
+
+array.forEach(
+    button => {
+        button.addEventListener('click', (e) => {
+            string += button.innerText
+            input.value = string
+        })
+    }
+)
